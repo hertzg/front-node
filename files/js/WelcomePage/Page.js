3f7eae8ddb0b06dbe1c829dc9d8ce60d4a6a8f36
Archil Imnadze <archil@imnadze.ge>
2016-05-26 14:46:30 +0000

    signup button

--- files/js/WelcomePage/Page.js
+++ files/js/WelcomePage/Page.js
@@ -6,24 +6,34 @@ function WelcomePage_Page () {
 
     var passwordItem = WelcomePage_PasswordItem()
 
-    var button = document.createElement('button')
-    button.className = classPrefix + '-button'
-    button.appendChild(document.createTextNode('Sign In'))
+    var signInButton = document.createElement('button')
+    signInButton.className = classPrefix + '-signInButton'
+    signInButton.appendChild(document.createTextNode('Sign In'))
 
     var signInFormElement = document.createElement('div')
     signInFormElement.className = classPrefix + '-signInForm'
     signInFormElement.appendChild(usernameItem.element)
     signInFormElement.appendChild(passwordItem.element)
-    signInFormElement.appendChild(button)
+    signInFormElement.appendChild(signInButton)
 
     var logoElement = document.createElement('img')
     logoElement.className = classPrefix + '-logo'
     logoElement.src = 'img/logo.svg'
 
+    var logoWrapperElement = document.createElement('div')
+    logoWrapperElement.className = classPrefix + '-logoWrapper'
+    logoWrapperElement.appendChild(logoElement)
+
+    var signUpButton = document.createElement('button')
+    signUpButton.className = classPrefix + '-signUpButton'
+    signUpButton.appendChild(document.createTextNode('Create an Account'))
+
     var centerElement = document.createElement('div')
     centerElement.className = classPrefix + '-center'
-    centerElement.appendChild(logoElement)
+    centerElement.appendChild(logoWrapperElement)
     centerElement.appendChild(signInFormElement)
+    centerElement.appendChild(document.createTextNode('New to Bazgu?'))
+    centerElement.appendChild(signUpButton)
 
     var element = document.createElement('div')
     element.className = classPrefix
