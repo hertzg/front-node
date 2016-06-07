e63ae8ab8d5318001e697a04dda067bbfc939aa8
Archil Imnadze <archil@imnadze.ge>
2016-06-07 07:46:04 +0000

    separate done messages and sending messages in message list

--- files/js/WorkPage/ChatPanel/Messages.js
+++ files/js/WorkPage/ChatPanel/Messages.js
@@ -3,18 +3,26 @@ function WorkPage_ChatPanel_Messages (session, username,
 
     var classPrefix = 'WorkPage_ChatPanel_Messages'
 
+    var doneMessagesElement = document.createElement('div')
+    doneMessagesElement.className = classPrefix + '-doneMessages'
+
+    var sendingMessagesElement = document.createElement('div')
+    sendingMessagesElement.className = classPrefix + '-sendingMessages'
+
     var contentElement = document.createElement('div')
     contentElement.className = classPrefix + '-content'
+    contentElement.appendChild(doneMessagesElement)
+    contentElement.appendChild(sendingMessagesElement)
 
     var typePanel = WorkPage_ChatPanel_TypePanel(function (text) {
 
         var sendingMessage = WorkPage_ChatPanel_SendingMessage(session, username, text, function () {
             var sentMessage = WorkPage_ChatPanel_SentMessage(text)
-            contentElement.removeChild(sendingMessage.element)
-            contentElement.appendChild(sentMessage.element)
+            sendingMessagesElement.removeChild(sendingMessage.element)
+            doneMessagesElement.appendChild(sentMessage.element)
         }, signOutListener, crashListener, serviceErrorListener)
 
-        contentElement.appendChild(sendingMessage.element)
+        sendingMessagesElement.appendChild(sendingMessage.element)
         contentElement.scrollTop = contentElement.scrollHeight
 
     }, closeListener)
@@ -29,12 +37,12 @@ function WorkPage_ChatPanel_Messages (session, username,
         focus: typePanel.focus,
         receiveTextMessage: function (text) {
             var message = WorkPage_ChatPanel_ReceivedMessage(text)
-            contentElement.appendChild(message.element)
+            doneMessagesElement.appendChild(message.element)
             contentElement.scrollTop = contentElement.scrollHeight
         },
         sendTextMessage: function (text) {
             var message = WorkPage_ChatPanel_SentMessage(text)
-            contentElement.appendChild(message.element)
+            doneMessagesElement.appendChild(message.element)
             contentElement.scrollTop = contentElement.scrollHeight
         },
     }
